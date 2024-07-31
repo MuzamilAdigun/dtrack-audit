082dd284fff30a0d9e8a51b9b742e32cd62eedcd
MuzamilAdigun <myself@muzamil-k.xyz>
2024-07-31 10:52:56 +0200

    add feature for vulnerabilities analysis

--- cmd/dtrack-audit/main.go
+++ cmd/dtrack-audit/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/ozontech/dtrack-audit/internal/dtrack"
 	"os"
+	"strings"
+
+	//	"reflect"
+	//	"strings"
 	"time"
+
+	"github.com/ozontech/dtrack-audit/internal/dtrack"
 )
 
 func checkError(e error) {
 	if e != nil {
 		fmt.Printf("[Dtrack Audit Error]: %s\n", e)
 		os.Exit(0)
+	}
+}
+func checkErro(e error) {
+	if e != nil {
+		fmt.Printf("fichu: %s\n", e)
+		//os.Exit(0)
 	}
 }
 
@@ -46,7 +57,6 @@ func main() {
 	if uploadResult.Token != "" {
 		fmt.Printf("SBOM file is successfully uploaded to DTrack API. Result token is %s\n", uploadResult.Token)
 	}
-
 	// In Sync mode we're waiting for findings from Dtrack
 	if uploadResult.Token != "" && config.SyncMode {
 		fmt.Println("In Sync mode we're waiting for findings from DTrack")
@@ -56,17 +66,60 @@ func main() {
 
 		findings, err := apiClient.GetFindings(config.ProjectId, config.SeverityFilter)
 		checkError(err)
+		s := 0
+		x := 0
+		fmt.Printf("%d vulnerabilities found!\n\n", len(findings))
+		//Anal, err := apiClient.VulnAnalysis(f.Comp.Uuid, f.Vuln.Uuid, config.ProjectId)
+		var finalString []string
+		for _, f := range findings {
+			urll, err := apiClient.VulURL(f.Vuln.Source, f.Vuln.VulnId)
+			checkError(err)
+			Analys, err := apiClient.VulnAnalysis(f.Comp.Uuid, f.Vuln.Uuid, config.ProjectId)
+			//checkErro(err)
+			var Ana string = "tt"
+			switch Analys.AnalysisState {
+			case "EXPLOITABLE":
+				Ana = "[Untreated] Exploitable Vulnerability set as - " + Analys.AnalysisState
+				s++
+			case "IN_TRIAGE":
+				Ana = "[Untreated] Investigation is in progress set as - " + Analys.AnalysisState
+				s++
+			case "NOT_SET":
+				Ana = "[Treated] Analysis has not commenced set as - " + Analys.AnalysisState
+				s++
+			case "NOT_AFFECTED":
+				Ana = "[Treated] set as - " + Analys.AnalysisState
+				x++
+			case "RESOLVED":
+				Ana = "[Treated] Vulnerability resolved  as - " + Analys.AnalysisState
+				x++
+			case "FALSE_POSITIVE":
+				Ana = "[Treated] Vulnerability was identified through faulty logic or data set as - " + Analys.AnalysisState
+				x++
 
-		if config.UseTeamCityOutput {
-			dtrack.PrintForTeamCity(findings, config)
-		} else {
-			dtrack.PrintForUser(findings, config)
+			default:
+				Ana = "[Untreated] Analysis has not commenced"
+				s++
+			}
+
+			finalString = append(finalString, fmt.Sprintf(
+				" > %s:  %s\n   Component: %s %s\n   More info: %s\n   State: %s\n\n",
+				f.Vuln.Severity, f.Vuln.Title, f.Comp.Name, f.Comp.Version, urll, Ana))
+
 		}
-		// For CI/CD integration
-		// Break corresponding CI/CD job to make developers 
-		// pay attention to findings
-		if len(findings) > 0 {
+		strings.Join(finalString[:], "")
+		fmt.Print(finalString)
+		fmt.Printf("\n\n")
+		if x > 0 {
+			fmt.Printf("%d Treated vulnerabilities !\n\n", x)
+
+		}
+
+		if s > 0 {
+			fmt.Printf("%d Untreated vulnerabilities !\n\n", s)
 			os.Exit(1)
+
 		}
+
 	}
 }
